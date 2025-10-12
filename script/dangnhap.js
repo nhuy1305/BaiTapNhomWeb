92143dc83992ffb895a82062156196367cd2d5e3
thoaivi45 <thoaivi45@gmail.com>
2025-10-12 19:07:44 +0700

    Update 12/10

--- script/dangnhap.js
+++ script/dangnhap.js
@@ -3,3 +3,7 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
   alert("Đăng nhập thành công!");
   window.location.href = "index.html";
 });
+
+localStorage.setItem('isLoggedIn', 'true');
+localStorage.setItem('userPhone', document.getElementById('email').value); // Tùy chỉnh field
+window.location.href = 'index.html'; // Hoặc quay lại trang trước
